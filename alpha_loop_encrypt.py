1be48eef07b5416392f8742fe67343eca1a16937
Mystical-serpent <yungteach1933@gmail.com>
2024-11-02 18:02:01 +0545

    Add files via upload
    This custom encryption method, designed with the Python programming language, encrypts messages using the position of each letter and a key that is required for both encrypting and decrypting the message.

---
+++ alpha_loop_encrypt.py
@@ -0,0 +1,58 @@
+Dict_of_store = {}
+each_element = []
+Dict_of_store_scrap = {}
+inp = []
+key =[]
+i = 0
+j = 26
+r = 0
+l_case = 'abcdefghijklmnopqrstuvwxyz'
+U_case = 'ABCDEFGHIJKLMNOPQRSTUVQXYZ'
+encrypted = ''
+
+for el_l in l_case:
+    Dict_of_store[el_l] = i+1
+    Dict_of_store_scrap[i+1] = el_l
+
+for el_U in U_case:
+    Dict_of_store[el_U] = j+1
+    Dict_of_store_scrap[j+1] = el_U
+
+
+Dict_of_store[" "]= 53
+
+inp_main = input("Main: ").strip()
+key_main = input("Key: ").strip()
+
+for el_main1 in inp_main:
+    inp.append(el_main1)
+
+if len(inp_main)>len(key_main):
+    for el_main2 in key_main:
+        key.append(el_main2)
+    while len(key)<len(inp):
+        key.append(key[i % len(key)])
+        i += 1
+
+elif len(key_main)>len(inp_main):
+    for el_main3 in key_main:
+        key.append(el_main3)
+        if len(key) == len(inp):
+            break
+else:
+    for el_main4 in key_main:
+        key.append(el_main4)
+
+for lock,key_elem in zip(inp, key):
+    another = Dict_of_store[lock] + Dict_of_store[key_elem]
+    each_element.append(another)
+
+for new_main in each_element:
+    if new_main>53:
+        num = new_main-53
+        encrypted += Dict_of_store_scrap[num]
+    else:
+        encrypted+= Dict_of_store_scrap[new_main]
+
+print(encrypted)
+
