94c4b8d7391b9956dd3d273877712f75f6154ca6
Mystical-serpent <yungteach1933@gmail.com>
2024-11-02 18:13:52 +0545

    Update alpha_loop_encrypt.py

--- alpha_loop_encrypt.py
+++ alpha_loop_encrypt.py
@@ -6,20 +6,20 @@ key =[]
 i = 0
 j = 26
 r = 0
-l_case = 'abcdefghijklmnopqrstuvwxyz'
+l_case = 'abcdefghijklmnopqrstuvwxyz'  #done so that the values are added automatically
 U_case = 'ABCDEFGHIJKLMNOPQRSTUVQXYZ'
 encrypted = ''
 
-for el_l in l_case:
+for el_l in l_case:    #adding lowercase letters to Dict_of_store
     Dict_of_store[el_l] = i+1
-    Dict_of_store_scrap[i+1] = el_l
+    Dict_of_store_scrap[i+1] = el_l   
 
-for el_U in U_case:
+for el_U in U_case:    #adding uppercase letters to Dict_of_store
     Dict_of_store[el_U] = j+1
     Dict_of_store_scrap[j+1] = el_U
 
 
-Dict_of_store[" "]= 53
+Dict_of_store[" "]= 53 
 
 inp_main = input("Main: ").strip()
 key_main = input("Key: ").strip()
@@ -27,7 +27,7 @@ key_main = input("Key: ").strip()
 for el_main1 in inp_main:
     inp.append(el_main1)
 
-if len(inp_main)>len(key_main):
+if len(inp_main)>len(key_main): #checks whether the key is the same length as the main
     for el_main2 in key_main:
         key.append(el_main2)
     while len(key)<len(inp):
@@ -43,16 +43,20 @@ else:
     for el_main4 in key_main:
         key.append(el_main4)
 
-for lock,key_elem in zip(inp, key):
+for lock,key_elem in zip(inp, key): #encrypting algorithm 
     another = Dict_of_store[lock] + Dict_of_store[key_elem]
     each_element.append(another)
-
+    
 for new_main in each_element:
-    if new_main>53:
-        num = new_main-53
-        encrypted += Dict_of_store_scrap[num]
+    if new_main > 53:
+        num = new_main - 53
+        if num in Dict_of_store_scrap:  
+            encrypted += Dict_of_store_scrap[num]
+        else:
+            encrypted += "?"  
     else:
-        encrypted+= Dict_of_store_scrap[new_main]
+        encrypted += Dict_of_store_scrap[new_main]
+
 
 print(encrypted)
 
